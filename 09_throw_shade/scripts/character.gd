f8a96cccc916c4e998325bef5cd54ea5df6c49c7
Nate <laffan@users.noreply.github.com>
2024-11-25 11:55:22 -0800

    reworked goshade project. Added viz shader nodes

--- 09_throw_shade/scripts/character.gd
+++ 09_throw_shade/scripts/character.gd
@@ -1,26 +1,18 @@
-# Godot's default character template.
-# no changes have been made.
-
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var horizontal := Input.get_axis("ui_left", "ui_right")
+	var vertical := Input.get_axis("ui_up", "ui_down")
+	
+	if vertical:
+		velocity.y = vertical * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if horizontal:
+		velocity.x = horizontal * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
